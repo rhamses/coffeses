41e1322d374902f99f8f76141791ce081eb982f1
Rhamses <rhamses.soares@gmail.com>
2020-06-26 03:07:35 -0300

    webmanifest fix

--- worker.js
+++ worker.js
@@ -30,8 +30,7 @@ self.addEventListener('install', function(event){
 				'/index.html',
 				'/images/icons/icon-192x192.png',
 				'https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css',
-				'https://fonts.googleapis.com/css2?family=Hind+Siliguri&display=swap',
-				'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4b4eb83e-cd2b-414e-99d3-90f9e4004d69/0001_AMBIENTE1_ICONE_%281%29.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200622%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200622T230508Z&X-Amz-Expires=86400&X-Amz-Signature=9e0a8723ccb72c41154bacd163186729cb7d8aeef72027c4c8b5773943c4d767&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%220001_AMBIENTE1_ICONE_%281%29.png%22'
+				'https://fonts.googleapis.com/css2?family=Hind+Siliguri&display=swap'
 			])
 		})
 	)
@@ -53,6 +52,7 @@ self.addEventListener('fetch', function(event){
 			})
 		}
 	}).catch(err => {
-		return caches.match('/images/icons/icon-192x192.png')
+		console.log("ruim", err);
+		// return caches.match('/images/icons/icon-192x192.png')
 	}));
 });
